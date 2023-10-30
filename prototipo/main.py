7339aea2fd4c002979244be182bfdc3b022884bf
gnunesvianaa <nunesvianagustavo@gmail.com>
2023-10-30 20:08:22 -0300

    refactor: main

--- prototipo/main.py
+++ prototipo/main.py
@@ -1,11 +1,12 @@
-import os, sys
+import os
+import sys
+
+# redefine o caminho de pesquisa
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from data.Jogo import Jogo
 
-# path padrao absoluto 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
- 
-# instanciacao e inicio do controle
+# inicia o jogo
 if __name__ == '__main__':
     jogo = Jogo()
     jogo.iniciar()
- 
