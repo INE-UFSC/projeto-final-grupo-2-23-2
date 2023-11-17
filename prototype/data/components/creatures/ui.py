7f47da03bdd87234c461762ba163586b9978e339
gnunesvianaa <nunesvianagustavo@gmail.com>
2023-11-16 22:22:13 -0300

    partial inventory ui

--- prototype/data/components/creatures/ui.py
+++ prototype/data/components/creatures/ui.py
@@ -10,7 +10,7 @@ class Ui():
 
     def show_health_bar(self):
         # bg rect
-        bg_rect = pygame.Rect(10, 10, 200, 20) 
+        bg_rect = pygame.Rect(13, 13, 300, 30) 
         pygame.draw.rect(self.display_surface, "#222222", bg_rect) 
         
         # insider rect
@@ -20,10 +20,24 @@ class Ui():
         current_rect.width = current_width
 
         pygame.draw.rect(self.display_surface, "red", current_rect)
-        pygame.draw.rect(self.display_surface, "#111111", bg_rect, 4)
+        pygame.draw.rect(self.display_surface, "#111111", bg_rect, 5)
 
     def show_inventory(self):
-        pass
+        inventory = self.controller.player.inventory
+
+        # bg rect
+        size = 80 
+        width = size * inventory.size()
+        bg_rect = pygame.Rect(13, self.display_surface.get_height() - size - 13, width, size)
+        pygame.draw.rect(self.display_surface, "#222222", bg_rect)
+
+        # item rect
+        for i in range(inventory.size()):
+            item_width, item_height = inventory.items[i].image.get_size()
+            x_position = bg_rect.left + (size * i) + (size - item_width) // 2
+            y_position = bg_rect.top + (size - item_height) // 2
+            self.display_surface.blit(inventory.items[i].image, (x_position, y_position))
+            pygame.draw.rect(self.display_surface, "#111111", bg_rect, 5)
 
     def display(self):
         self.show_health_bar()
